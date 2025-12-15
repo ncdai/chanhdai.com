73f675695dc7708f90e52e5a8f98c9fcdd0b0c51
thefoxcost <extractiongames@gmail.com>
2025-12-15 11:36:02 -0800

    Added a wllpaper in place of the logo and changed the theme to DOOM theme

--- src/features/portfolio/components/profile-cover.tsx
+++ src/features/portfolio/components/profile-cover.tsx
@@ -1,5 +1,6 @@
+import Image from "next/image";
+
 import { BrandContextMenu } from "@/components/brand-context-menu";
-import { ChanhDaiMark } from "@/components/chanhdai-mark";
 import { cn } from "@/lib/utils";
 
 export function ProfileCover() {
@@ -13,9 +14,11 @@ export function ProfileCover() {
           "bg-black/0.75 bg-[radial-gradient(var(--pattern-foreground)_1px,transparent_0)] bg-size-[10px_10px] bg-center [--pattern-foreground:var(--color-zinc-950)]/5 dark:bg-white/0.75 dark:[--pattern-foreground:var(--color-white)]/5"
         )}
       >
-        <ChanhDaiMark
-          id="js-cover-mark"
-          className="h-14 w-28 sm:h-16 sm:w-32"
+        <Image
+          src="https://4kwallpapers.com/images/walls/thumbs_3t/20831.jpg"
+          width={1000}
+          height={1000}
+          alt="Doom wallpaper"
         />
       </div>
     </BrandContextMenu>
