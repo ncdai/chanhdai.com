f88600e8ec32fa5abe2f06e67362dd2690ddf1bb
Nguyễn Chánh Đại <dai@chanhdai.com>
2026-02-19 20:03:31 +0700

    feat: refactor social links component and create new social link item component

--- src/app/(app)/(root)/page.tsx
+++ src/app/(app)/(root)/page.tsx
@@ -12,7 +12,7 @@ import { Overview } from "@/features/portfolio/components/overview"
 import { ProfileCover } from "@/features/portfolio/components/profile-cover"
 import { ProfileHeader } from "@/features/portfolio/components/profile-header"
 import { Projects } from "@/features/portfolio/components/projects"
-import { SocialLinks } from "@/features/portfolio/components/social-links"
+import { SocialLinks } from "@/features/portfolio/components/social-links-v2"
 import { Sponsors } from "@/features/portfolio/components/sponsors"
 import { TechStack } from "@/features/portfolio/components/tech-stack"
 import { TestimonialsMarquee } from "@/features/portfolio/components/testimonials-marquee"
@@ -35,8 +35,6 @@ export default function Page() {
         <Separator />
 
         <Overview />
-        <Separator />
-
         <SocialLinks />
         <Separator />
 
