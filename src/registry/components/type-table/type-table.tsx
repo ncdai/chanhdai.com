f42cb7e15b46b58bc3167b3cf77dc40848e1fbb2
Nguyễn Chánh Đại <dai@chanhdai.com>
2026-02-20 16:16:26 +0700

    feat: update dependencies and introduce new auto-type table component with markdown rendering capabilities

---
+++ src/registry/components/type-table/type-table.tsx
@@ -0,0 +1,196 @@
+"use client"
+
+import { ChevronDown } from "lucide-react"
+import type { ComponentProps, ReactNode } from "react"
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { Prose } from "@/components/ui/typography"
+import { cn } from "@/lib/utils"
+
+export interface ParameterNode {
+  name: string
+  description: ReactNode
+}
+
+export interface TypeNode {
+  /**
+   * Additional description of the field
+   */
+  description?: ReactNode
+
+  /**
+   * type signature (short)
+   */
+  type: ReactNode
+
+  /**
+   * type signature (full)
+   */
+  typeDescription?: ReactNode
+
+  /**
+   * Optional `href` for the type
+   */
+  typeDescriptionLink?: string
+
+  default?: ReactNode
+
+  required?: boolean
+  deprecated?: boolean
+
+  /**
+   * a list of parameters info if the type is a function.
+   */
+  parameters?: ParameterNode[]
+
+  returns?: ReactNode
+}
+
+export function TypeTable({
+  id,
+  type,
+  className,
+  ...props
+}: ComponentProps<"div"> & { type: Record<string, TypeNode> }) {
+  return (
+    <div
+      id={id}
+      className={cn(
+        "@container my-[1.25em] flex flex-col gap-px overflow-hidden rounded-xl bg-code p-1 text-sm",
+        className
+      )}
+      {...props}
+    >
+      <div className="not-prose flex items-center px-3 py-2 font-medium text-muted-foreground">
+        <p className="w-1/4">Prop</p>
+        <p className="@max-xl:hidden">Type</p>
+      </div>
+
+      {Object.entries(type).map(([key, value]) => (
+        <Item key={key} parentId={id} name={key} item={value} />
+      ))}
+    </div>
+  )
+}
+
+function Item({
+  parentId,
+  name,
+  item: {
+    parameters = [],
+    description,
+    required = false,
+    deprecated,
+    typeDescription,
+    default: defaultValue,
+    type,
+    typeDescriptionLink,
+    returns,
+  },
+}: {
+  parentId?: string
+  name: string
+  item: TypeNode
+}) {
+  const id = parentId ? `${parentId}-${name}` : undefined
+
+  return (
+    <Collapsible
+      id={id}
+      className="rounded-lg ring-border ring-inset data-open:bg-background data-open:ring-1 data-open:not-last:mb-0.75"
+    >
+      <CollapsibleTrigger className="group not-prose relative flex w-full flex-row items-center rounded-lg px-3 py-2 text-start text-sm ring-border outline-none ring-inset hover:bg-accent focus-visible:ring-1 focus-visible:ring-ring/50 dark:hover:bg-[color-mix(in_oklab,var(--accent)_60%,var(--code))] data-open:rounded-b-none data-open:ring-1">
+        <code
+          className={cn(
+            "[--shiki-dark:#79B8FF] [--shiki-light:#005CC5]",
+            "w-1/4 min-w-fit pe-2 font-mono font-medium text-(--shiki-light) dark:text-(--shiki-dark)",
+            deprecated && "text-primary/50 line-through"
+          )}
+        >
+          {name}
+          {!required && "?"}
+        </code>
+
+        {typeDescriptionLink ? (
+          <a
+            href={typeDescriptionLink}
+            className="underline underline-offset-4 @max-xl:hidden"
+          >
+            {type}
+          </a>
+        ) : (
+          <span className="@max-xl:hidden">{type}</span>
+        )}
+
+        <ChevronDown className="absolute inset-e-2 size-4 text-muted-foreground group-data-[state=open]:rotate-180" />
+      </CollapsibleTrigger>
+
+      <CollapsibleContent className="overflow-hidden">
+        <div className="grid grid-cols-[1fr_3fr] gap-y-3 p-3 text-sm">
+          <CustomProse className="col-span-full empty:hidden">
+            {description}
+          </CustomProse>
+
+          {typeDescription && (
+            <>
+              <p className="not-prose pe-2 text-muted-foreground">Type</p>
+              <p className="not-prose my-auto">{typeDescription}</p>
+            </>
+          )}
+
+          {defaultValue && (
+            <>
+              <p className="not-prose pe-2 text-muted-foreground">Default</p>
+              <p className="not-prose my-auto">{defaultValue}</p>
+            </>
+          )}
+
+          {parameters.length > 0 && (
+            <>
+              <p className="not-prose pe-2 text-muted-foreground">Parameters</p>
+              <div className="flex flex-col gap-2">
+                {parameters.map((param) => (
+                  <div
+                    key={param.name}
+                    className="inline-flex flex-wrap items-center gap-1"
+                  >
+                    <p className="not-prose font-medium text-nowrap">
+                      {param.name} –
+                    </p>
+                    <CustomProse>{param.description}</CustomProse>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+
+          {returns && (
+            <>
+              <p className="not-prose pe-2 text-muted-foreground">Returns</p>
+              <CustomProse className="my-auto">{returns}</CustomProse>
+            </>
+          )}
+        </div>
+      </CollapsibleContent>
+    </Collapsible>
+  )
+}
+
+function CustomProse({
+  className,
+  ...props
+}: React.ComponentProps<typeof Prose>) {
+  return (
+    <Prose
+      className={cn(
+        "prose-sm prose-no-margin prose-code:py-[1.5px] prose-code:text-[13px]",
+        className
+      )}
+      {...props}
+    />
+  )
+}
