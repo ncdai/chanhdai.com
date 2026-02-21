c994c4532110f0e04db9b1c0f4105efd7bd8d9ca
Nguyễn Chánh Đại <dai@chanhdai.com>
2026-02-21 12:15:55 +0700

    fix(social-links.ts): update Discord URL to use HTTPS for secure connection

--- src/features/portfolio/data/social-links.ts
+++ src/features/portfolio/data/social-links.ts
@@ -29,7 +29,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
     icon: "https://assets.chanhdai.com/images/link-icons/discord.webp?v=2",
     title: "Discord",
     subtitle: "ncdai",
-    href: "http://discord.com/users/1186630645443739651",
+    href: "https://discord.com/users/1186630645443739651",
   },
   // {
   //   icon: "https://assets.chanhdai.com/images/link-icons/zalo.webp?v=2",
