3c5b02f93d1f5e1b6aca4f573d060172629eb166
Nguyễn Chánh Đại <dai@chanhdai.com>
2026-03-01 12:43:51 +0700

    chore: update @openpanel/web and @openpanel/sdk dependencies to version 1.1.0; configure session replay options in ConsentManagerClient

--- src/lib/openpanel.ts
+++ src/lib/openpanel.ts
@@ -3,4 +3,5 @@ import { OpenPanel } from "@openpanel/web"
 export const op = new OpenPanel({
   clientId: process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID!,
   trackScreenViews: true,
+  disabled: true,
 })
