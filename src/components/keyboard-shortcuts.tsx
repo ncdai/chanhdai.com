e7de862321cbaad27018768e69b51e3c512df04d
Nguyễn Chánh Đại <dai@chanhdai.com>
2026-03-29 21:54:08 +0700

    refactor(command-menu): replace 'posts' with 'docs' for clarity and consistency feat(command-menu): add 'Wall of Love' link with shortcut and icon fix(command-menu): update icon handling to use React elements feat(icons): add 'bookHeart' icon for 'Wall of Love' feat(keyboard-shortcuts): add shortcut for 'Wall of Love' page refactor(site-header): rename 'posts' to 'docs' for improved clarity

--- src/components/keyboard-shortcuts.tsx
+++ src/components/keyboard-shortcuts.tsx
@@ -11,6 +11,7 @@ export function KeyboardShortcuts() {
   useHotkeys("g>b", () => router.push("/blocks"))
   useHotkeys("g>l", () => router.push("/blog"))
   useHotkeys("g>s", () => router.push("/sponsors"))
+  useHotkeys("g>w", () => router.push("/wall-of-love"))
 
   return null
 }
